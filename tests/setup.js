5910c9d1059c87da6f6a03aeb5016109c5514394
thiagoaolvdev <thiagoaolv.dev@gmail.com>
2026-08-15 19:29:19 -0300

    Delete tests directory

--- tests/setup.js
+++
@@ -1,6 +0,0 @@
-require('dotenv').config({ path: require('path').join(__dirname, '..', '.env') });
-process.env.NODE_ENV = 'test';
-process.env.DB_NAME = 'chemarauto_test';
-process.env.JWT_SECRET = 'test_secret';
-process.env.MAIL_USER = '';
-process.env.MAIL_PASS = '';
